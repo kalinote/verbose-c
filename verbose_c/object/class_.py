6af9a3dd06ae545be31d81b2dcec429caa5032b6
kalinote <kalinote@163.com>
2025-06-17 12:15:49 +0800

    完善类与实例对象

--- verbose_c/object/class_.py
+++ verbose_c/object/class_.py
@@ -1,4 +1,5 @@
 from verbose_c.object.enum import VBCObjectType
+from verbose_c.object.instance import VBCInstance
 from verbose_c.object.object import VBCObject
 
 
@@ -6,23 +7,34 @@ class VBCClass(VBCObject):
     """
     verbose-c 类类
     """
-    def __init__(self, name: str, super_class: list[str] = []):
+    def __init__(self, name: str, super_class: list["VBCClass"] = []):
         super().__init__(VBCObjectType.CLASS)
-        self._name: str = name                           # 类名
-        self._super_class: list[str] = super_class       # 父类名列表
-        self._methods: dict[str, VBCObject] = {}         # 方法字典
-        self._fields: dict[str, VBCObject] = {}          # 字段字典
+        self._name: str = name                                  # 类名
+        self._super_class: list["VBCClass"] = super_class       # 父类名列表
+        self._methods: dict[str, VBCObject] = {}                # 方法字典
+        self._fields: dict[str, VBCObject] = {}                 # 字段字典
 
     def __str__(self):
         return super().__str__() + f"(name={self._name})"
 
-    def create_instance(self):
+    def create_instance(self) -> VBCInstance:
         """
-        TODO 实例化类
+        实例化类
         """
-        pass
+        return VBCInstance(class_=self)
 
-    def lookup_method(self):
+    def lookup_method(self, name: str) -> VBCObject | None:
         """
-        TODO 查找方法
+        查找方法
         """
+        method = self._methods.get(name)
+        if method:
+            return method
+
+        for sclass in self._super_class:
+            method = sclass.lookup_method(name)
+            if method:
+                return method
+
+        return None
+    
--- verbose_c/object/instance.py
+++ verbose_c/object/instance.py
@@ -6,24 +6,32 @@ class VBCInstance(VBCObject):
     """
     verbose-c 类的实例对象
     """
-    def __init__(self, klass):
+    def __init__(self, class_):
+        from verbose_c.object.class_ import VBCClass
         super().__init__(VBCObjectType.INSTANCE)
         # 所属类
-        self.klass = klass
+        self.class_: VBCClass = class_
         # 实例字段和值
         self.fields: dict[str, VBCObject] = {}
 
     def __repr__(self) -> str:
-        return f"<instance of {self.klass._name}>"
+        return self.__str__()
 
     def __str__(self):
-        return super().__str__() + f"(instance of {self.klass._name})"
+        return super().__str__() + f"(instance of {self.class_._name})"
 
     def get_attribute(self, name: str) -> 'VBCObject | None':
         """
-        TODO 获取实例属性
+        获取实例属性
         """
-        pass
+        if name in self.fields:
+            return self.fields[name]
+
+        method = self.class_.lookup_method(name)
+        if method:
+            return method
+
+        return None
 
     def set_attribute(self, name: str, value: 'VBCObject'):
         """设置实例的属性"""
